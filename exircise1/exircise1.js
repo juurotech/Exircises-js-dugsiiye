9a2285a78db0c48e6573f2ea67c79b2a42db89b4
juurotech <juurotech@gamail.com>
2025-12-19 05:39:51 +0300

    submited exircise 1

---
+++ exircise1/exircise1.js
@@ -0,0 +1,15 @@
+let x =7;
+let y =3;
+
+
+console.log (x+y);
+console.log (x-y);
+console.log (x*y)
+console.log (x/y)
+console.log (x%y)
+console.log (x++)
+console.log (++x)
+console.log ("post increment")
+
+console.log ("pre increment")
+console.log (x)
