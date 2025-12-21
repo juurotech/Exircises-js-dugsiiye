0d283e9fe371a3a3bc2dcb784213a6f61b48c3c2
juurotech <juurotech@gamail.com>
2025-12-21 04:55:19 +0300

    submiteed exircise-4

---
+++ Exircise-4/exircise-4.js
@@ -0,0 +1,19 @@
+function add (a,b ) {
+    return a+b;
+}
+console.log (add(3,5))
+console.log (add(4,8))
+console.log (add(80,100))
+console.log (add(20,0.5))
+
+
+
+const greet = function(name){
+    console.log ("helooo "   + name)
+}
+
+
+greet( "abdulakdir gardhub")
+greet("raham ali")
+greet("xuseen dhicisow")
+
