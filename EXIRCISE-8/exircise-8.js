0df5d735d478033f38e9153204da7746dc3e66b5
juurotech <juurotech@gamail.com>
2025-12-21 12:56:59 +0300

    submited exircise-8

---
+++ EXIRCISE-8/exircise-8.js
@@ -0,0 +1,15 @@
+let car = {
+    make : 2019,
+    model :"Tayota",
+    year :2020
+    
+     
+
+};
+
+console.log (car.make)
+console.log (car.model)
+console.log (car.year)
+
+
+console.log (car["make"])
