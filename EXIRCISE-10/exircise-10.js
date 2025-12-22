5fa850235cd8cd5e46ef3b2e950ebbf47cb3de83
juurotech <juurotech@gamail.com>
2025-12-22 04:51:46 +0300

    submited exircise-10

---
+++ EXIRCISE-10/exircise-10.js
@@ -0,0 +1,16 @@
+let library =[
+    {
+        title :"hadafka nolosha",
+        author :"Abdukadir Gardhub",
+        year :2024,
+    },
+    {
+        titele :"arimaha qoyska",
+        author :"Abduakdir Gardhub",
+        year :2023,
+    }
+]
+
+
+console.log (library[0].title);
+console.log (library[1].author)
