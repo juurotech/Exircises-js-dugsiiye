d127634ed0affc2251c6ad7173904b9e6387be8b
juurotech <juurotech@gamail.com>
2026-01-05 11:46:08 +0300

    submited exircise 32

---
+++ EXIRCISE-32/exircise-32.js
@@ -0,0 +1,9 @@
+const  title = document.querySelector("#title")
+
+console.log (title)
+const  list = document.querySelector(".list")
+console.log(list)
+const  p = document.querySelector("p")
+console.log (p)
+const button = document.querySelector(button)
+console.log (button)
